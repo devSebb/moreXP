ac3dbee146d23be240579bd712018eb5b2107063
Sebastian Burke <burke.sebastian1@gmail.com>
2024-08-12 21:57:31 +0800

    industry selection functionality for new job

---
+++ app/javascript/controllers/industry_dropdown_controller.js
@@ -0,0 +1,19 @@
+// app/javascript/controllers/industry_dropdown_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["selected", "output"]
+
+  selectItem(event) {
+    event.preventDefault()
+    const selectedValue = event.target.getAttribute('data-value')
+    this.selectedTarget.textContent = selectedValue
+    this.outputTarget.textContent = selectedValue
+    this.outputTarget.value = selectedValue
+    const hiddenField = document.getElementById('industry-hidden-field');
+    hiddenField.value = selectedValue;
+
+    const dropdownMenu = this.element.querySelector('[data-industry-dropdown-target="selected"]');
+    dropdownMenu.classList.add('hidden');
+  }
+}
