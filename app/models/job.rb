1484d24a6e4cb098447075450bd0b5c247cfa938
Sebastian Burke <burke.sebastian1@gmail.com>
2024-08-14 16:09:05 +0800

    fixed jobCard colors bug

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,14 @@ class Job < ApplicationRecord
   validates :title, :description, :company_name, :location, :price, :industry, presence: true
   has_many :applications
   belongs_to :user
+
+  INDUSTRY_COLORS = {
+    "Software Development" => '#f5f3c4',
+    "Finance & Accounting" => '#c4dcf5',
+    "Architecture" => '#c4f5c6',
+    "Marketing & Graphic Design" => '#f5c4e1',
+    "Photography" => '#c4c6f5',
+    "Video & Film" => '#c4f5c5',
+    "Fashion" => '#f5dac4'
+  }
 end
