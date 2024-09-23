6ff5af5f2044a78cf6fd574324c8a9899e88ca9f
Sebastian Burke <burke.sebastian1@gmail.com>
2024-09-23 16:43:14 +0700

    Fix asset paths and precompile assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.turbo.min.js"
 pin "anime", to: "https://cdn.skypack.dev/animejs@3.2.1"
+pin "home_animation", to: "home_animation.js", preload: true
+pin "path_animation", to: "path_animation.js", preload: true
